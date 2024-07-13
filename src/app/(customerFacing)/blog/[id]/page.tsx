72212f9baaec0b24d08c59b9aa79873b781314ff
Ashura <ashuramajestic@gmail.com>
2024-07-13 10:48:51 +0530

    ashura

--- src/app/(customerFacing)/blog/[id]/page.tsx
+++ src/app/(customerFacing)/blog/[id]/page.tsx
@@ -1,3 +1,4 @@
+"use server";
 import db from "@/db/db";
 import BlogDetail from "./_components/BlogDetail";
 export default async function BlogShowPage({
@@ -6,14 +7,18 @@ export default async function BlogShowPage({
   params: { id: string };
 }) {
   const data = await db.post.findUnique({ where: { id } });
-  return (
-    <BlogDetail
-      username={data?.username}
-      title={data?.title}
-      introduction={data?.introduction}
-      content={data?.content}
-      minutes={data?.minutes}
-      createdAt={data?.createdAt}
-    />
-  );
+  if (!data) {
+    return <>Error</>;
+  } else {
+    return (
+      <BlogDetail
+        username={data?.username}
+        title={data?.title}
+        introduction={data?.introduction}
+        content={data?.content}
+        minutes={data?.minutes}
+        createdAt={data?.createdAt}
+      />
+    );
+  }
 }
