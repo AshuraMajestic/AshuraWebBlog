e77ceb4752f0ec072ad71e3e8a075eea60966e31
Ashura <ashuramajestic@gmail.com>
2024-07-23 11:40:17 +0530

    ashura

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-Z3668DY1B5"
+        ></script>
+        <script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-Z3668DY1B5');
+          `}
+        </script>
+      </head>
       <ThemeProvider>
         <body
           className={cn(
